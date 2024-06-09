e5ced64fae0ffc7bdd635404d9149a6445eb9263
Anttoni Maula <manttoni92@gmail.com>
2024-06-09 15:40:19 +0300

    it is somehow sorting arrays now

--- push_swap.c
+++ push_swap.c
@@ -12,17 +12,38 @@ void	print_stack(t_stack *stack)
 
 void	p(t_stack *a, t_stack *b)
 {
+	printf("========\n");
 	printf("Stack a:\n");
 	print_stack(a);
 	printf("Stack b:\n");
 	print_stack(b);
 }
 
+int	is_ascending(t_stack *stack)
+{
+	int	i;
+
+	i = 0;
+	while (i < stack->len - 1)
+	{
+		if (stack->numbers[i] > stack->numbers[i + 1])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 void	push_swap(t_stack *a, t_stack *b)
 {
-	p(a, b);
-	rotate(a);
+	while (!is_ascending(a) || !is_ascending(b))
+	{
+		p(a, b);
+		if (a->numbers[0] > a->numbers[1])
+			swap(a);
+		push(a, b);
+		if (a->len == 0)
+			while (b->len != 0)
+				push(b, a);
+	}
 	p(a,b);
-	rotate_reverse(a);
-	p(a, b);
 }
