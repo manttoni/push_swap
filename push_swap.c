5c488f1a8a1b5765c4acf6e615d4ce07392d35f3
Anttoni Maula <manttoni92@gmail.com>
2024-06-10 17:57:03 +0300

    cleaning

--- push_swap.c
+++ push_swap.c
@@ -1,38 +1,4 @@
 #include "push_swap.h"
-#include <stdio.h>
-#include <time.h>
-
-void	p(t_stack *a, t_stack *b)
-{
-	int i = 0;
-
-	while (i < a->len || i < b->len)
-	{
-		if (i < a->len)
-			printf("%d\t", a->numbers[i]);
-		else
-			printf("  \t");
-		if (i < b->len)
-			printf("%d", b->numbers[i]);
-		printf("\n");
-		i++;
-	}
-	printf("==========\na\tb\n------------------------\n");
-}
-
-int	is_ascending(t_stack *stack)
-{
-	int	i;
-
-	i = 0;
-	while (i < stack->len - 1)
-	{
-		if (stack->numbers[i] > stack->numbers[i + 1])
-			return (0);
-		i++;
-	}
-	return (1);
-}
 
 /* Compare top of stacks
  * Return
@@ -51,7 +17,6 @@ void	push_swap(t_stack *a, t_stack *b)
 	int	ops;
 
 	ops = 0;
-	p(a,b);
 	while (a->len != 0)
 	{
 		if (compare(a, b) >= 0)
@@ -61,5 +26,5 @@ void	push_swap(t_stack *a, t_stack *b)
 	}
 	while (b->len != 0)
 		ops += push(b, a);
-	printf("Ops: %d\nTime: %fms\n", ops, 1000 * ((double)clock() / CLOCKS_PER_SEC));
+	ft_printf("Ops: %d\n", ops);
 }
