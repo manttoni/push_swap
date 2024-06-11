427f6c20380dfbeb105dc4c55d6a9454f6083f21
Anttoni Maula <manttoni92@gmail.com>
2024-06-11 18:51:09 +0300

    another version, sometimes faster

--- push_swap.c
+++ push_swap.c
@@ -2,8 +2,23 @@
 #include <time.h>
 #include <stdio.h>
 
+int	is_ascending(t_stack *stack)
+{
+	int	i;
+
+	i = 0;
+	while (i < stack->len - 1)
+	{
+		if (stack->numbers[i] > stack->numbers[i + 1])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 void	p(t_stack *a, t_stack *b)
 {
+	return ;
 	ft_printf("\n\ta:");
 	for (int i = 0; i < a->len; ++i)
 		ft_printf(" %d", a->numbers[i]);
@@ -55,13 +70,60 @@ int	algo(t_stack *a, t_stack *b)
 	return (ops);
 }
 
+int	algo2(t_stack *a, t_stack *b)
+{
+	int	ops;
+	int	max;
 
+	max = INT_MIN;
+	ops = 0;
+	p(a, b);
+	while (a->len != 1)
+	{
+		if(a->numbers[0] > max)
+			max = a->numbers[0];
+		ops += push(a, b);
+		p(a, b);
+	}
+	while (b->numbers[0] != max && b->len > 1)
+		ops += rotate(b);
+	ft_printf("--------------------------------------");
+	while (b->len != 0)
+	{
+		while (a->numbers[0] < b->numbers[0] && a->len >= 2)
+		{
+			ops += rotate(a);
+			p(a, b);
+		}
+		ops += push(b, a);
+		p(a, b);
+		while (a->numbers[0] > a->numbers[a->len - 1] && b->numbers[0] < a->numbers[a->len - 1])
+		{
+			ops += rotate_reverse(a);
+			p(a, b);
+		}
+	}
+
+
+	return (ops);
+}
 
 void	push_swap(t_stack *a, t_stack *b)
 {
-	int	ops;
 
-	ops = algo(a, b);
-	ft_printf("\nOps: %d\n", ops);
-	printf("Time: %g ms\n", 1000 * ((double)clock() / CLOCKS_PER_SEC));
+	int	*array = malloc(a->len * sizeof(int));
+	for (int i = 0; i < a->len; ++i)
+		array[i] = a->numbers[i];
+	t_stack	*copy = malloc(sizeof(t_stack));
+	copy->numbers = array;
+	copy->len = a->len;
+
+	ft_printf("\nAlgo1:");
+	ft_printf("\n\tOps: %d\n", algo(a, b));
+	p(a, b);
+	ft_printf("\nAlgo2:");
+	ft_printf("\n\tOps: %d\n", algo2(copy, b));
+	p(copy, b);
+	free(array);
+	free(copy);
 }
