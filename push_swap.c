1797f4e0682263fe6c63c224abbe92d2f58ab81d
Anttoni Maula <amaula@c1r4p8.hive.fi>
2024-06-26 17:35:04 +0300

    bugfix

--- push_swap.c
+++ push_swap.c
@@ -6,9 +6,8 @@ void	push_swap(t_stack *a, t_stack *b, t_recorder *recorder)
 		ft_printf("Error");
 	for (unsigned int i = 0; i < recorder->len; i++)
 		ft_printf("%s\n", recorder->operations[i]);
-	ft_printf("\n\nOps: %u\n", recorder->len);
-	for (unsigned int i = 0; i < a->len; i++)
-		ft_printf("%d ", a->numbers[i]);
-	ft_printf("\n");
+	//ft_printf("\n\nOps: %u\n", recorder->len);
+	//for (unsigned int i = 0; i < a->len; i++)
+	//	ft_printf("%d ", a->numbers[i]);
 }
 
