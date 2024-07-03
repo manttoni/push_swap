fa5c8a99eaa2fa4c2284fd7f8b74e155ace05370
Anttoni Maula <manttoni92@gmail.com>
2024-07-03 14:30:49 +0300

    least_rotations optimized, but small input result worse

--- magic_sort_utils.c
+++ magic_sort_utils.c
@@ -1,39 +1,13 @@
 #include "push_swap.h"
 
-int	stack_max(t_stack *stack)
-{
-	int				max;
-	unsigned int	i;
-
-	i = 0;
-	max = INT_MIN;
-	while (i < stack->len)
-	{
-		if (top(stack, i) > max)
-			max = top(stack, i);
-		i++;
-	}
-	return (max);
-}
-
-int	stack_min(t_stack *stack)
-{
-	int				min;
-	unsigned int	i;
-
-	i = 0;
-	min = INT_MAX;
-	while (i < stack->len)
-	{
-		if (top(stack, i) < min)
-			min = top(stack, i);
-		i++;
-	}
-	return (min);
-}
-
 static int	is_aligned(t_stack *a, t_stack *b, int i, int j)
 {
+	if (b->len == 0)
+	{
+		if (top(a, i) < top(a, i - 1))
+			return (1);
+		return (0);
+	}
 	if (top(b, j) < top(a, i) && top(b, j) > top(a, i - 1))
 		return (1);
 	if (top(b, j) < top(a, i) && top(a, i - 1) == stack_max(a))
@@ -43,28 +17,22 @@ static int	is_aligned(t_stack *a, t_stack *b, int i, int j)
 	return (0);
 }
 
-static void	update_rotations(int *rotations, int i, int j, unsigned int *least)
+static unsigned int	get_rotations(int i, int j)
 {
-	int				same_sign;
+	int		both_negative;
+	int		both_positive;
 	unsigned int	greater;
 
-	greater = absolute(i);
-	if (absolute(j) > absolute(i))
-		greater = absolute(j);
-	same_sign = rotations[0] < 0 && rotations[1] < 0;
-	same_sign = same_sign || (rotations[0] > 0 && rotations[1] > 0);
-	if (same_sign && *least > greater)
+	both_negative = i < 0 && j < 0;
+	both_positive = i > 0 && j > 0;
+	if (both_negative || both_positive)
 	{
-		*least = greater;
-		rotations[0] = i;
-		rotations[1] = j;
+		greater = absolute(i);
+		if (absolute(j) > absolute(i))
+			greater = absolute(j);
+		return (greater);
 	}
-	else if (*least > absolute(i) + absolute(j))
-	{
-		*least = absolute(i) + absolute(j);
-		rotations[0] = i;
-		rotations[1] = j;
-	}
+	return (absolute(i) + absolute(j));
 }
 
 void	least_rotations(t_stack *a, t_stack *b, int *rotations)
@@ -80,13 +48,14 @@ void	least_rotations(t_stack *a, t_stack *b, int *rotations)
 		j = -1 * (long)b->len / 2;
 		while (j <= (long)(b->len / 2))
 		{
-			if (b->len == 0 && top(a, i) < top(a, i - 1))
-				update_rotations(rotations, i, 0, &least);
-			else if (b->len != 0 && is_aligned(a, b, i, j))
-				update_rotations(rotations, i, j, &least);
+			if (get_rotations(i, j) < least && is_aligned(a, b, i, j))
+			{
+				least = get_rotations(i, j);
+				rotations[0] = i;
+				rotations[1] = j;
+			}
 			j++;
 		}
 		i++;
 	}
 }
-
--- stack_utils.c
+++ stack_utils.c
@@ -40,3 +40,35 @@ unsigned int	absolute(int i)
 		return (-i);
 	return (i);
 }
+
+int     stack_max(t_stack *stack)
+{
+        int                             max;
+        unsigned int    i;
+
+        i = 0;
+        max = INT_MIN;
+        while (i < stack->len)
+        {
+                if (top(stack, i) > max)
+                        max = top(stack, i);
+                i++;
+        }
+        return (max);
+}
+
+int     stack_min(t_stack *stack)
+{
+        int                             min;
+        unsigned int    i;
+
+        i = 0;
+        min = INT_MAX;
+        while (i < stack->len)
+        {
+                if (top(stack, i) < min)
+                        min = top(stack, i);
+                i++;
+        }
+        return (min);
+}
